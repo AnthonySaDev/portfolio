3a16319677fb97515ea01eb890bd61dc512f1c08
AnthonySaDev <anthonysareis11@gmail.com>
2024-09-20 10:49:04 -0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,182 @@
+export const navItems = [
+  { name: "Início", link: "#home" },
+  { name: "Sobre", link: "#about" },
+  { name: "Experiência", link: "#experience" },
+  { name: "Contato", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Prioridade na colaboração com o cliente, promovendo comunicação aberta",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexibilidade de horário para te atender em qualquer momento",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tecnologias",
+    description: "Estou sempre em busca de aprimoramento",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de tecnologia com paixão por desenvolvimento.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Desenvolvendo App do Money Wise",
+    description: "Aplicativo focado em auxiliar o planejamento financeiro.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Quer iniciar um projeto comigo?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "MoneyWise",
+    des: "Um aplicativo financeiro inteligente para ajudar as pessoas a gerenciar suas finanças e melhorar seu planejamento financeiro.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://github.com/AnthonySaDev/financeApp",
+  },
+  {
+    id: 2,
+    title: "NovoCred",
+    des: "Desenvolvimento de um aplicativo de crédito voltado para os clientes da Minas Brasil, com foco em usabilidade e integração financeira.",
+    img: "/p2.svg",
+    iconLists: ["/re.svg", "/ts.svg"],
+    link: "https://github.com/AnthonySaDev/NovoCred",
+  },
+  {
+    id: 3,
+    title: "LarDasVelhinhas",
+    des: "Site desenvolvido para ajudar o lar das velhinhas de Montes Claros - MG.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://lardasvelhinhas.com.br/",
+  },
+  {
+    id: 4,
+    title: "Queluz",
+    des: "Desenvolvimento completo do site institucional da QueluzMoc, com foco em performance e experiência do usuário.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "https://www.queluzmoc.com.br/",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Desenvolvedor Fullstack - Devvo",
+    desc: "Atuei no desenvolvimento de aplicações web e móveis, tanto no frontend com ReactJS, Nextjs, React Native e Expo quanto no backend com NodeJs, Express, Fastify e Strapi e bancos de dados relacionais e não relacionais.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Desenvolvedor Mobile - Prodemge",
+    desc: "Manutenção de código em React Native para o sistema REDS, implementação de novos recursos, resolução de bugs, otimização de desempenho, análise desenvolvimento de documentação técnica e testes com Jest e React Testing Library.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Desenvolvedor Web - Nobilis Marketing",
+    desc: "Migração de projeto web de wordpress para React, construção de painéis administrativos utilizando React e Firebase, planejamento e estimativa de projetos, trabalhando em modelo ágil.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Freelancer - Desenvolvimento de Aplicativos e Sites",
+    desc: "Crio sites e aplicativos que atenda as necessidades dos meus clientes, utilizando ReactJS, React Native e NodeJS, com prazos curtos e atendimento personalizado",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    link: "https://github.com/AnthonySaDev",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    link: "https://www.linkedin.com/in/anthony-sa-reis/",
+    img: "/link.svg",
+  },
+];
